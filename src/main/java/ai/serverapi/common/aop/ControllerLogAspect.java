e37605796045351efb09e9c801dd8e94b8c43e99
head1ton <head1ton@gmail.com>
2023-10-26 00:44:40 +0900

    LogAspect -> ControllerLogAspect 로 변경

---
+++ src/main/java/ai/serverapi/common/aop/ControllerLogAspect.java
@@ -0,0 +1,62 @@
+package ai.serverapi.common.aop;
+
+import ai.serverapi.domain.dto.ErrorApi;
+import ai.serverapi.domain.enums.ResultCode;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllerLogAspect {
+
+    @Around("execution(* ai.serverapi.controller..*.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        String type = pjp.getSignature().getDeclaringTypeName();
+        String method = pjp.getSignature().getName();
+        String requestURI = ((ServletRequestAttributes) requestAttributes).getRequest()
+                                                                          .getRequestURI();
+
+        log.info("[logging] validation check... requestUri = [{}] package = [{}], method = [{}]",
+            requestURI, type, method);
+
+        Object[] args = pjp.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof final BindingResult bindingResult) {
+                if (bindingResult.hasErrors()) {
+                    List<String> errors = new ArrayList<>();
+                    for (FieldError error : bindingResult.getFieldErrors()) {
+                        log.warn("[parameter : {}] [message = {}]", error.getField(),
+                            error.getDefaultMessage());
+                        errors.add(String.format("%s", error.getDefaultMessage()));
+                    }
+
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                         .body(
+                                             ErrorApi.<String>builder()
+                                                     .code(ResultCode.BAD_REQUEST.CODE)
+                                                     .message(ResultCode.BAD_REQUEST.MESSAGE)
+                                                     .errors(errors)
+                                                     .build()
+                                         );
+                }
+            }
+        }
+
+        return pjp.proceed();
+    }
+
+}

72fc048e1b701c216151bf0de340bbffca48eaf6
head1ton <head1ton@gmail.com>
2023-10-26 00:47:24 +0900

    ServiceLogAspect 생성

---
+++ src/main/java/ai/serverapi/common/aop/ServiceLogAspect.java
@@ -0,0 +1,20 @@
+package ai.serverapi.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ServiceLogAspect {
+
+    @Around("execution(* ai.serverapi.service..*.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("[logging] method = [{}]", pjp.getSignature().toShortString());
+
+        return pjp.proceed();
+    }
+}

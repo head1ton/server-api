19e0707e9898c6db39e33bc48feadb01e6af4b24
head1ton <head1ton@gmail.com>
2023-10-26 16:01:17 +0900

    RestdocsConfig 로직 생성

--- src/test/java/ai/serverapi/config/RestdocsConfig.java
+++ src/test/java/ai/serverapi/config/RestdocsConfig.java
@@ -1,5 +1,26 @@
 package ai.serverapi.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.snippet.Attributes;
+import org.springframework.restdocs.snippet.Attributes.Attribute;
+
+@TestConfiguration
 public class RestdocsConfig {
 
+    public static final Attribute field(String key, String value) {
+        return new Attribute(key, value);
+    }
+
+    @Bean
+    public RestDocumentationResultHandler handler() {
+        return MockMvcRestDocumentation.document(
+            "{class-name}/{method-name}",
+            Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+            Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+        );
+    }
 }

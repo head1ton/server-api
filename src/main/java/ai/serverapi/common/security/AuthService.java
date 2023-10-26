f632c18b7b169640d8cf33ec7561e994aa5a2abd
head1ton <head1ton@gmail.com>
2023-10-26 21:32:47 +0900

    AuthService 하드코딩 되어 있는 부분 수정

--- src/main/java/ai/serverapi/common/security/AuthService.java
+++ src/main/java/ai/serverapi/common/security/AuthService.java
@@ -1,5 +1,12 @@
 package ai.serverapi.common.security;
 
+import ai.serverapi.domain.entity.member.Member;
+import ai.serverapi.domain.enums.Role;
+import ai.serverapi.repository.member.MemberRepository;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,17 +15,30 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class AuthService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         log.info("회원 인증 처리");
 
+        Member member = memberRepository.findByEmail(username).orElseThrow(() ->
+            new UsernameNotFoundException("유효하지 않은 회원입니다."));
+
+        Role role = member.getRole();
+        Set<String> roleSet = new HashSet<>();
+        roleSet.add("USER");
+        roleSet.add(role.ROLE);
+
+        String[] roles = Arrays.copyOf(roleSet.toArray(), roleSet.size(), String[].class);
+
         return User.builder()
-                   .username(String.valueOf(1L))
-                   .password("$2a$10$HCA6Vr7SCVW6FRg6Thk2BeXAbhgFd/MfeR1m6EaeXA64sN0S5vaXG")
-                   .roles("USER", "SELLER")
+                   .username(String.valueOf(member.getId()))
+                   .password(member.getPassword())
+                   .roles(roles)
                    .build();
     }
 }

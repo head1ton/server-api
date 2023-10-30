d9da41a8efcdd873bcfffac92c19bbb70c197ad4
head1ton <head1ton@gmail.com>
2023-10-31 01:15:42 +0900

    상품 검색 api 추가 & 문서화

--- src/main/java/ai/serverapi/controller/product/ProductController.java
+++ src/main/java/ai/serverapi/controller/product/ProductController.java
@@ -3,17 +3,22 @@ package ai.serverapi.controller.product;
 import ai.serverapi.domain.dto.Api;
 import ai.serverapi.domain.dto.product.ProductDto;
 import ai.serverapi.domain.enums.ResultCode;
+import ai.serverapi.domain.vo.product.ProductListVo;
 import ai.serverapi.domain.vo.product.ProductVo;
 import ai.serverapi.service.product.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,5 +39,17 @@ public class ProductController {
                                       .message(ResultCode.POST.message)
                                       .data(productService.postProduct(productDto, request))
                                       .build());
+    }
+
+    @GetMapping
+    public ResponseEntity<Api<ProductListVo>> getProduct(
+        @PageableDefault(size = 10) Pageable pageable,
+        @RequestParam(name = "search") String search
+    ) {
+        return ResponseEntity.ok(Api.<ProductListVo>builder()
+                                    .code(ResultCode.SUCCESS.code)
+                                    .message(ResultCode.SUCCESS.message)
+                                    .data(productService.getProductList(pageable, search))
+                                    .build());
     }
 }
--- src/test/java/ai/serverapi/controller/product/ProductControllerDocs.java
+++ src/test/java/ai/serverapi/controller/product/ProductControllerDocs.java
@@ -3,32 +3,44 @@ package ai.serverapi.controller.product;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import ai.serverapi.BaseTest;
 import ai.serverapi.domain.dto.member.LoginDto;
 import ai.serverapi.domain.dto.product.ProductDto;
+import ai.serverapi.domain.entity.member.Member;
+import ai.serverapi.domain.entity.product.Product;
 import ai.serverapi.domain.vo.member.LoginVo;
+import ai.serverapi.repository.member.MemberRepository;
+import ai.serverapi.repository.product.ProductRepository;
 import ai.serverapi.service.member.MemberAuthService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ProductControllerDocs extends BaseTest {
 
     private static final String PREFIX = "/api/seller/product";
 
     @Autowired
     private MemberAuthService memberAuthService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
     @DisplayName(PREFIX)
@@ -91,6 +103,92 @@ class ProductControllerDocs extends BaseTest {
                 fieldWithPath("data.image1").type(JsonFieldType.STRING).description("이미지1"),
                 fieldWithPath("data.image2").type(JsonFieldType.STRING).description("이미지2"),
                 fieldWithPath("data.image3").type(JsonFieldType.STRING).description("이미지3")
+            )
+        ));
+    }
+
+    @Test
+    @DisplayName(PREFIX)
+    void getProduct() throws Exception {
+        LoginDto loginDto = new LoginDto(SELLER_EMAIL, PASSWORD);
+        LoginVo login = memberAuthService.login(loginDto);
+
+        Member member = memberRepository.findByEmail(SELLER_EMAIL).get();
+
+        ProductDto productDto = new ProductDto("메인 제목", "메인 설명", "상품 메인 설명", "상품 서브 설명", 10000,
+            8000, "보관 방법", "원산지", "생산자", "https://mainImage", "https://image1", "htts://image2",
+            "https://image3");
+
+        ProductDto searchDto = new ProductDto("검색 제목", "메인 설명", "상품 메인 설명", "상품 서브 설명", 10000,
+            8000, "보관 방법", "원산지", "생산자", "https://mainImage", "https://image1", "htts://image2",
+            "https://image3");
+
+        productRepository.save(Product.of(member, searchDto));
+
+        for (int i = 0; i < 25; i++) {
+            productRepository.save(Product.of(member, productDto));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            productRepository.save(Product.of(member, searchDto));
+        }
+
+        ResultActions perform = mockMvc.perform(
+            get(PREFIX)
+                .header(AUTHORIZATION, "Bearer " + login.getAccessToken())
+                .param("search", "검색").param("page", "0").param("size", "5")
+        );
+
+        perform.andExpect(status().is2xxSuccessful());
+
+        perform.andDo(docs.document(
+            requestHeaders(
+                headerWithName(AUTHORIZATION).description("access token")
+            ),
+            queryParameters(
+                parameterWithName("page").description("paging 시작 페이지 번호"),
+                parameterWithName("size").description("paging 시작 페이지 기준 개수 크기"),
+                parameterWithName("search").description("검색어")
+            ),
+            responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description("결과 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메세지"),
+                fieldWithPath("data.total_page").type(JsonFieldType.NUMBER).description("페이지 총 개수"),
+                fieldWithPath("data.total_elements").type(JsonFieldType.NUMBER)
+                                                    .description("총 데이터수"),
+                fieldWithPath("data.number_of_elements").type(JsonFieldType.NUMBER)
+                                                        .description("페이지 총 데이터수"),
+                fieldWithPath("data.last").type(JsonFieldType.BOOLEAN)
+                                          .description("페이지 마지막 데이터 여부"),
+                fieldWithPath("data.empty").type(JsonFieldType.BOOLEAN)
+                                           .description("페이지 데이터 존재 여부"),
+                fieldWithPath("data.list[]").type(JsonFieldType.ARRAY).description("등록 상품 list"),
+                fieldWithPath("data.list[].id").type(JsonFieldType.NUMBER).description("등록 상품 id"),
+                fieldWithPath("data.list[].main_title").type(JsonFieldType.STRING)
+                                                       .description("메인 타이틀"),
+                fieldWithPath("data.list[].main_explanation").type(JsonFieldType.STRING)
+                                                             .description("메인 설명"),
+                fieldWithPath("data.list[].product_main_explanation").type(JsonFieldType.STRING)
+                                                                     .description("상품 메인 설명"),
+                fieldWithPath("data.list[].product_sub_explanation").type(JsonFieldType.STRING)
+                                                                    .description("상품 서브 설명"),
+                fieldWithPath("data.list[].origin_price").type(JsonFieldType.NUMBER)
+                                                         .description("상품 원가"),
+                fieldWithPath("data.list[].price").type(JsonFieldType.NUMBER)
+                                                  .description("상품 실제 판매 가격"),
+                fieldWithPath("data.list[].purchase_inquiry").type(JsonFieldType.STRING)
+                                                             .description("취급 방법"),
+                fieldWithPath("data.list[].origin").type(JsonFieldType.STRING).description("원산지"),
+                fieldWithPath("data.list[].producer").type(JsonFieldType.STRING).description("공급자"),
+                fieldWithPath("data.list[].main_image").type(JsonFieldType.STRING)
+                                                       .description("메인 이미지 url"),
+                fieldWithPath("data.list[].image1").type(JsonFieldType.STRING).description("이미지1"),
+                fieldWithPath("data.list[].image2").type(JsonFieldType.STRING).description("이미지2"),
+                fieldWithPath("data.list[].image3").type(JsonFieldType.STRING).description("이미지3"),
+                fieldWithPath("data.list[].created_at").type(JsonFieldType.STRING)
+                                                       .description("등록일"),
+                fieldWithPath("data.list[].modified_at").type(JsonFieldType.STRING)
+                                                        .description("수정일")
             )
         ));
     }

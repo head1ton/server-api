044c013cebdbe7ac6ee78657415f7067a5e65ce1
head1ton <head1ton@gmail.com>
2023-10-31 00:17:31 +0900

    querydsl 상품 검색 기능 추가

--- src/test/java/ai/serverapi/service/product/ProductServiceTest.java
+++ src/test/java/ai/serverapi/service/product/ProductServiceTest.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import ai.serverapi.domain.dto.member.LoginDto;
 import ai.serverapi.domain.dto.product.ProductDto;
+import ai.serverapi.domain.entity.member.Member;
 import ai.serverapi.domain.entity.product.Product;
 import ai.serverapi.domain.vo.member.LoginVo;
+import ai.serverapi.domain.vo.product.ProductListVo;
 import ai.serverapi.domain.vo.product.ProductVo;
+import ai.serverapi.repository.member.MemberRepository;
 import ai.serverapi.repository.product.ProductRepository;
 import ai.serverapi.service.member.MemberAuthService;
 import java.util.Optional;
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +32,8 @@ class ProductServiceTest {
     @Autowired
     private ProductRepository productRepository;
     private final MockHttpServletRequest request = new MockHttpServletRequest();
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("상품 등록 성공")
@@ -62,6 +68,35 @@ class ProductServiceTest {
 
         assertThat(productId).isNotEmpty();
         assertThat(memberEmail).isEqualTo(loginDto.getEmail());
+    }
+
+    @Test
+    @DisplayName("상품 리스트 불러오기")
+    void getProductList() {
+        Member member = memberRepository.findByEmail("seller@gmail.com").get();
+
+        ProductDto productDto = new ProductDto("메인 제목", "메인 설명", "상품 메인 설명", "상품 서브 설명", 10000,
+            8000, "보관 방법", "원산지", "생산자", "https://mainImage", null, null, null);
+
+        ProductDto searchDto = new ProductDto("검색 제목", "메인 설명", "상품 메인 설명", "상품 서브 설명", 10000,
+            8000, "보관 방법", "원산지", "생산자", "https://mainImage", null, null, null);
+
+        productRepository.save(Product.of(member, searchDto));
+
+        for (int i = 0; i < 25; i++) {
+            productRepository.save(Product.of(member, productDto));
+        }
+        for (int i = 0; i < 10; i++) {
+            productRepository.save(Product.of(member, searchDto));
+        }
+
+        Pageable pageable = Pageable.ofSize(5);
+        pageable = pageable.next();
+
+        ProductListVo searchList = productService.getProductList(pageable, "검색");
+
+        assertThat(searchList.getList().stream().findFirst().get().getMainTitle()).contains("검색");
+
 
     }
 

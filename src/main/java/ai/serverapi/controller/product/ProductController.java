d3d77517f54764e39c6e5ea07fce13f6c2cb903c
head1ton <head1ton@gmail.com>
2023-10-31 15:06:07 +0900

    상품 목록 api 권한 문제로 수정

--- src/main/java/ai/serverapi/controller/product/ProductController.java
+++ src/main/java/ai/serverapi/controller/product/ProductController.java
@@ -1,45 +1,24 @@
 package ai.serverapi.controller.product;
 
 import ai.serverapi.domain.dto.Api;
-import ai.serverapi.domain.dto.product.ProductDto;
 import ai.serverapi.domain.enums.ResultCode;
 import ai.serverapi.domain.vo.product.ProductListVo;
-import ai.serverapi.domain.vo.product.ProductVo;
 import ai.serverapi.service.product.ProductService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("${api-prefix}/seller/product")
+@RequestMapping("${api-prefix}/product")
 public class ProductController {
 
     private final ProductService productService;
-
-    @PostMapping
-    public ResponseEntity<Api<ProductVo>> postProduct(
-        @RequestBody @Validated ProductDto productDto,
-        HttpServletRequest request,
-        BindingResult bindingResult) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(Api.<ProductVo>builder()
-                                      .code(ResultCode.POST.code)
-                                      .message(ResultCode.POST.message)
-                                      .data(productService.postProduct(productDto, request))
-                                      .build());
-    }
 
     @GetMapping
     public ResponseEntity<Api<ProductListVo>> getProduct(
--- src/test/java/ai/serverapi/controller/product/ProductControllerDocs.java
+++ src/test/java/ai/serverapi/controller/product/ProductControllerDocs.java
@@ -1,12 +1,7 @@
 package ai.serverapi.controller.product;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
@@ -26,14 +21,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ProductControllerDocs extends BaseTest {
 
-    private static final String PREFIX = "/api/seller/product";
+    private static final String PREFIX = "/api/product";
 
     @Autowired
     private MemberAuthService memberAuthService;
@@ -41,71 +35,6 @@ class ProductControllerDocs extends BaseTest {
     private MemberRepository memberRepository;
     @Autowired
     private ProductRepository productRepository;
-
-    @Test
-    @DisplayName(PREFIX)
-    void postProduct() throws Exception {
-        LoginDto loginDto = new LoginDto(SELLER_EMAIL, PASSWORD);
-        LoginVo login = memberAuthService.login(loginDto);
-        ProductDto productDto = new ProductDto("메인 타이틀", "메인 설명", "상품 메인 설명", "상품 서브 설명", 10000,
-            9000, "취급 방법", "원산지", "공급자", "https://main_image", "https://image1", "https://image2",
-            "https://image3");
-
-        ResultActions perform = mockMvc.perform(
-            post(PREFIX)
-                .header(AUTHORIZATION, "Bearer " + login.getAccessToken())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(objectMapper.writeValueAsString(productDto))
-        );
-
-        perform.andExpect(status().is2xxSuccessful());
-
-        perform.andDo(docs.document(
-            requestHeaders(
-                headerWithName(AUTHORIZATION).description("access token")
-            ),
-            requestFields(
-                fieldWithPath("main_title").type(JsonFieldType.STRING).description("제목"),
-                fieldWithPath("main_explanation").type(JsonFieldType.STRING).description("메인 설명"),
-                fieldWithPath("product_main_explanation").type(JsonFieldType.STRING)
-                                                         .description("상품 메인 설명"),
-                fieldWithPath("product_sub_explanation").type(JsonFieldType.STRING)
-                                                        .description("상품 보조 설명"),
-                fieldWithPath("origin_price").type(JsonFieldType.NUMBER).description("원가"),
-                fieldWithPath("price").type(JsonFieldType.NUMBER).description("실제 판매가"),
-                fieldWithPath("purchase_inquiry").type(JsonFieldType.STRING).description("취급방법"),
-                fieldWithPath("origin").type(JsonFieldType.STRING).description("원산지"),
-                fieldWithPath("producer").type(JsonFieldType.STRING).description("공급자"),
-                fieldWithPath("main_image").type(JsonFieldType.STRING).description("메인 이미지"),
-                fieldWithPath("image1").type(JsonFieldType.STRING).description("이미지1"),
-                fieldWithPath("image2").type(JsonFieldType.STRING).description("이미지2"),
-                fieldWithPath("image3").type(JsonFieldType.STRING).description("이미지3")
-            ),
-            responseFields(
-                fieldWithPath("code").type(JsonFieldType.STRING).description("결과 코드"),
-                fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메세지"),
-                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("등록 상품 id"),
-                fieldWithPath("data.main_title").type(JsonFieldType.STRING).description("메인 타이틀"),
-                fieldWithPath("data.main_explanation").type(JsonFieldType.STRING)
-                                                      .description("메인 설명"),
-                fieldWithPath("data.product_main_explanation").type(JsonFieldType.STRING)
-                                                              .description("상품 메인 설명"),
-                fieldWithPath("data.product_sub_explanation").type(JsonFieldType.STRING)
-                                                             .description("상품 서브 설명"),
-                fieldWithPath("data.origin_price").type(JsonFieldType.NUMBER).description("상품 원가"),
-                fieldWithPath("data.price").type(JsonFieldType.NUMBER).description("상품 실제 판매 가격"),
-                fieldWithPath("data.purchase_inquiry").type(JsonFieldType.STRING)
-                                                      .description("취급 방법"),
-                fieldWithPath("data.origin").type(JsonFieldType.STRING).description("원산지"),
-                fieldWithPath("data.producer").type(JsonFieldType.STRING).description("공급자"),
-                fieldWithPath("data.main_image").type(JsonFieldType.STRING)
-                                                .description("메인 이미지 url"),
-                fieldWithPath("data.image1").type(JsonFieldType.STRING).description("이미지1"),
-                fieldWithPath("data.image2").type(JsonFieldType.STRING).description("이미지2"),
-                fieldWithPath("data.image3").type(JsonFieldType.STRING).description("이미지3")
-            )
-        ));
-    }
 
     @Test
     @DisplayName(PREFIX)
@@ -135,16 +64,12 @@ class ProductControllerDocs extends BaseTest {
 
         ResultActions perform = mockMvc.perform(
             get(PREFIX)
-                .header(AUTHORIZATION, "Bearer " + login.getAccessToken())
                 .param("search", "검색").param("page", "0").param("size", "5")
         );
 
         perform.andExpect(status().is2xxSuccessful());
 
         perform.andDo(docs.document(
-            requestHeaders(
-                headerWithName(AUTHORIZATION).description("access token")
-            ),
             queryParameters(
                 parameterWithName("page").description("paging 시작 페이지 번호"),
                 parameterWithName("size").description("paging 시작 페이지 기준 개수 크기"),

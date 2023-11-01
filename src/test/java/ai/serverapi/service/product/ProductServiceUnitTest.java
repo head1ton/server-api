209521fbfcaaf187debbeecdf33fef38364130eb
head1ton <head1ton@gmail.com>
2023-11-02 01:55:37 +0900

    상품 수정 api 추가 및 문서

--- src/test/java/ai/serverapi/service/product/ProductServiceUnitTest.java
+++ src/test/java/ai/serverapi/service/product/ProductServiceUnitTest.java
@@ -7,6 +7,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import ai.serverapi.common.security.TokenProvider;
 import ai.serverapi.domain.dto.product.ProductDto;
+import ai.serverapi.domain.dto.product.PutProductDto;
 import ai.serverapi.domain.entity.member.Member;
 import ai.serverapi.domain.entity.product.Product;
 import ai.serverapi.domain.enums.Role;
@@ -81,4 +82,37 @@ class ProductServiceUnitTest {
         assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
                              .hasMessageContaining("유효하지 않은 상품");
     }
+
+    @Test
+    @DisplayName("수정하려는 상품이 존재하지 않는 경우 실패")
+    void putProductFail1() {
+        PutProductDto dto = new PutProductDto(0L, null, null, null, null, 0, 0,
+            null, null, null, null, null, null, null);
+        BDDMockito.given(productRepository.findById(any())).willReturn(Optional.ofNullable(null));
+
+        Throwable throwable = catchThrowable(() -> productService.putProduct(dto));
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
+                             .hasMessageContaining("유효하지 않은 상품");
+    }
+
+//    @Test
+//    @DisplayName("수정하려는 상품이 존재하지만 회원의 정보가 없을 경우엔 실패")
+//    void putProductFail2() {
+//        PatchProductDto dto = new PatchProductDto(1L, null, null, null, null, 0, 0, null, null,
+//            null, null, null, null, null);
+//
+//        BDDMockito.given(tokenProvider.resolveToken(any())).willReturn("token");
+//        LocalDateTime now = LocalDateTime.now();
+//        Member member = new Member(1L, "email@mail.com", "password", "nickname", "name",
+//            "19941030", Role.SELLER, null, null, now, now);
+//
+//        ProductDto productDto = new ProductDto();
+//        BDDMockito.given(productRepository.findById(any())).willReturn(Optional.ofNullable(Product.of(member, productDto)));
+//        BDDMockito.given(memberRepository.findById(any())).willReturn(Optional.ofNullable(null));
+//
+//        Throwable throwable = catchThrowable(() -> productService.putProduct(dto));
+//        System.out.println("throwable = " + throwable);
+////        assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
+////            .hasMessageContaining("유효하지 않은 회원");
+//    }
 }

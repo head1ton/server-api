656f3c6adbc905f437f44a0d2a56107b8171bf0e
head1ton <head1ton@gmail.com>
2023-11-04 01:16:33 +0900

    구매자 정보 추가 등록하지 못하도록 예외처리

--- src/test/java/ai/serverapi/service/member/MemberServiceUnitTest.java
+++ src/test/java/ai/serverapi/service/member/MemberServiceUnitTest.java
@@ -2,14 +2,19 @@ package ai.serverapi.service.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import ai.serverapi.common.security.TokenProvider;
 import ai.serverapi.domain.dto.member.PatchMemberDto;
 import ai.serverapi.domain.dto.member.PostBuyerInfoDto;
 import ai.serverapi.domain.dto.member.PutBuyerInfoDto;
+import ai.serverapi.domain.entity.member.BuyerInfo;
+import ai.serverapi.domain.entity.member.Member;
+import ai.serverapi.repository.member.BuyerInfoRepository;
 import ai.serverapi.repository.member.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,6 +31,8 @@ class MemberServiceUnitTest {
     HttpServletRequest request = new MockHttpServletRequest();
     @Mock
     private MemberRepository memberRepository;
+    @Mock
+    private BuyerInfoRepository buyerInfoRepository;
     @Mock
     private TokenProvider tokenProvider;
 
@@ -58,6 +65,26 @@ class MemberServiceUnitTest {
 
         assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
                              .hasMessageContaining("존재하지 않는 회원");
+    }
+
+    @Test
+    @DisplayName("이미 구매자 정보를 입력했을 경우 입력에 실패")
+    void postBuyerInfoFail2() {
+        PostBuyerInfoDto postBuyerInfoDto = new PostBuyerInfoDto("홍길동", "buyer_info@gmail.com",
+            "01012341234");
+
+        BDDMockito.given(tokenProvider.getMemberId(request)).willReturn(1L);
+        Member member = new Member(1L, null, null, null, null, null, null, null, null, null, null);
+
+        member.putBuyerInfo(BuyerInfo.of(null, "구매자", "buyer@gmail.com", "01012341234"));
+        BDDMockito.given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
+
+        Throwable throwable = catchThrowable(
+            () -> memberService.postBuyerInfo(postBuyerInfoDto, request));
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
+                             .hasMessageContaining("이미 구매자 정보를 등록");
+
     }
 
     @Test

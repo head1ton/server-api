b0c8f8344a6a56601a3015c0723f74a05effdc1c
head1ton <head1ton@gmail.com>
2023-11-09 18:08:33 +0900

    환경설정 & exception cause 추가

--- src/main/java/ai/serverapi/global/exception/DuringProcessException.java
+++ src/main/java/ai/serverapi/global/exception/DuringProcessException.java
@@ -5,4 +5,8 @@ public class DuringProcessException extends RuntimeException {
     public DuringProcessException(String message) {
         super(message);
     }
+
+    public DuringProcessException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }

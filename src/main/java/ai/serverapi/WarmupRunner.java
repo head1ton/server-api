e7454280a28a88a457ac4aa329f1a0754dfb973f
head1ton <head1ton@gmail.com>
2023-11-12 18:32:08 +0900

    warmup runner 초기화

--- src/main/java/ai/serverapi/WarmupRunner.java
+++ src/main/java/ai/serverapi/WarmupRunner.java
@@ -1,23 +1,18 @@
 package ai.serverapi;
 
-import ai.serverapi.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WarmupRunner implements ApplicationListener<ApplicationReadyEvent> {
 
-    public static boolean isWarmup = false;
-    private final ProductService productService;
-
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        Pageable pageable = Pageable.ofSize(10);
-        productService.getProductList(pageable, "", "normal", 0L, 0L);
-        isWarmup = true;
+        log.info("warmup");
     }
 }
